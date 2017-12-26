05f422f315e3ef067d1ae79e3e38927536303e9c
kyohei.minami <kyohei.minami@sacn.co.jp>
2017-12-26 14:18:24 +0900

    Initial Commit

---
+++ iOSHandOn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSHandOn
+//
+//  Created by kyohei.minami on 2017/12/26.
+//  Copyright © 2017年 handson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
